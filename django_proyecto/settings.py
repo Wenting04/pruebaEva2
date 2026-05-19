e67e032ee891299e92deef59ef5fe6ad59a0b2f2
Wenting04 <wentingzhang1928@gmail.com>
2026-05-19 11:44:50 +0200

    Configurar ruta STATIC_ROOT para el despliegue en producción
    SETTINGS
    - Añadir variable 'STATIC_ROOT' que apunta a 'staticfiles'
    - Para el comando 'collectstatic' compilar archivos a un único directorio para el servidor

--- django_proyecto/settings.py
+++ django_proyecto/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles' # Carpeta destino exclusivo para servidor real
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
